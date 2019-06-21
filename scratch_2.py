6bf895a67aadc3c18d377a7e91a9e80589084969
Sven Balnojan <sven.balnojan@mercateo.com>
2019-06-21 09:20:36 +0200

    fix typo

--- scratch_2.py
+++ scratch_2.py
@@ -148,8 +148,8 @@ print(scores_2)
 print("finally, let's do some ranking...")
 entity_count = 8
 scores, _, _ = comparator(
-    comparator.prepare(torch.tensor(src_embedding.reshape([1,1,10]))).expand(1, entity_count+1, 10),
-    comparator.prepare(torch.tensor(dest_embeddings.reshape([1,entity_count+1,10]))),
+    comparator.prepare(torch.tensor(src_embedding.reshape([1,1,10]))).expand(1, entity_count, 10),
+    comparator.prepare(torch.tensor(dest_embeddings.reshape([1,entity_count,10]))),
     torch.empty(1, 0, 10),  # Left-hand side negatives, not needed
     torch.empty(1, 0, 10),  # Right-hand side negatives, not needed
 )
